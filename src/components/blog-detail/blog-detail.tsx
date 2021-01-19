8b299696bf7b79ed44bb4302487017d71f400765
westlife0615 <westlife0615@gmail.com>
2021-01-19 19:41:04 +0900

    ADD harry porter

--- src/components/blog-detail/blog-detail.tsx
+++ src/components/blog-detail/blog-detail.tsx
@@ -11,6 +11,8 @@ export const BlogDetail = (props: DetailProps)=>{
             <h1>이거참 뿌듯하구만</h1><br></br>
              <h3>햄버거 맞나군</h3>
 
+            <h1>얍 !!!! </h1>
+            <img src={"https://i.pinimg.com/originals/79/b6/bf/79b6bf5eb2ddc9ffa920dc46fb681b29.gif"}></img>
             <table>
                 <tr>123</tr>
                 <tr>456</tr>
