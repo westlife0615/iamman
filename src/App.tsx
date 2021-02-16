1f909859c2c1cbde83595911d610ad8bf3f77480
westlife0615 <westlife0615@gmail.com>
2021-02-16 19:24:08 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -5,59 +5,58 @@ import {Header} from "./components/header/header";
 import SideNav from "./components/side-nav/SideNav";
 
 export type NavigationGroup = {
-    text: string,
-    component: JSX.Element
+  text: string,
+  path: string
 }
 
 export const INITIAL_NAV_GROUP_LIST: NavigationGroup[] = [
-    {
-        text: "게시글 리스트",
-        component: <div><img
-            src={"https://img.huffingtonpost.com/asset/5d708e2f2400004f0075b4e8.jpeg?ops=scalefit_630_noupscale"}
-            style={{width: "100%", height: 'auto'}}/></div>
-    },
-    {
-        text: "상세 페이지",
-        component: <div><img src={"http://ojsfile.ohmynews.com/STD_IMG_FILE/2011/0525/IE001309768_STD.jpg"}
-                             style={{width: "100%", height: 'auto'}}/></div>
-    },
-    {
-        text: "글쓰기",
-        component: <div><img src={"https://www.chrischae.kr/content/images/size/w2000/2019/12/SteveJobsCloseUp-2.jpeg"}
-                             style={{width: "100%", height: 'auto'}}/></div>
-    },
+  {
+    text: "게시글 리스트",
+    path: "/list"
+  },
+  {
+    text: "상세 페이지",
+    path: "/detail"
+  },
+  {
+    text: "글쓰기",
+    path: "/write"
+  },
 ]
 
+const List = () => {
+  return <div><img
+    src={ "https://img.huffingtonpost.com/asset/5d708e2f2400004f0075b4e8.jpeg?ops=scalefit_630_noupscale" }
+    style={ {width: "100%", height: 'auto'} }/></div>
+}
+
+const Detail = () => {
+  return <div><img src={ "http://ojsfile.ohmynews.com/STD_IMG_FILE/2011/0525/IE001309768_STD.jpg" }
+                   style={ {width: "100%", height: 'auto'} }/></div>
+}
+
+const Write = () => {
+  return <div><img src={ "https://www.chrischae.kr/content/images/size/w2000/2019/12/SteveJobsCloseUp-2.jpeg" }
+                   style={ {width: "100%", height: 'auto'} }/></div>
+}
+
 function App() {
-    let navigationGroupList: NavigationGroup[] = INITIAL_NAV_GROUP_LIST;
-
-    return (
-        <BrowserRouter basename={"/iamman"}>
-            <React.Fragment>
-                <SideNav navigationGroupList={navigationGroupList}>
-                    <Header/>
-                    <Route path={"/"} exact={true}>
-
-                    </Route>
-                    {/*<Route path={"/login"} exact={true}>*/}
-                    {/*    <React.Fragment>*/}
-                    {/*        <div style={{margin: "20px"}}>*/}
-                    {/*            <input type={"text"} placeholder={"이메일을 입력하세요."} style={{width: '90%', height: '30px', fontSize: '20px'}}/>*/}
-                    {/*        </div>*/}
-                    {/*        <div style={{margin: "20px"}}>*/}
-                    {/*            <input type={"password"} placeholder={"비밀번호를 입력하요."} style={{width: '90%', height: '30px', fontSize: '20px'}}/>*/}
-                    {/*        </div>*/}
-                    {/*        <div style={{margin: "20px"}}>*/}
-                    {/*            <button>로그인</button>*/}
-                    {/*        </div>*/}
-
-                    {/*    </React.Fragment>*/}
-                    {/*</Route>*/}
-                </SideNav>
-            </React.Fragment>
-
-        </BrowserRouter>
-    );
+  let navigationGroupList: NavigationGroup[] = INITIAL_NAV_GROUP_LIST;
+  
+  return (
+    <BrowserRouter basename={ "/iamman" }>
+      <React.Fragment>
+        <SideNav navigationGroupList={ navigationGroupList }>
+          <Header/>
+          <Route path={ "/" } exact={ true } component={ List }/>
+          <Route path={ "/list" } exact={ true } component={ List }/>
+          <Route path={ "/detail" } exact={ true } component={ Detail }/>
+          <Route path={ "/write" } exact={ true } component={ Write }/>
+        </SideNav>
+      </React.Fragment>
+    
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {useHistory, NavLink} from "react-router-dom";
 import {NavigationGroup} from "../../App";
 
 
@@ -7,53 +8,49 @@ import {NavigationGroup} from "../../App";
 import {css, jsx} from "@emotion/react";
 
 export interface SideNavProps {
-    navigationGroupList: NavigationGroup[],
-    children: JSX.Element[] | JSX.Element
+  navigationGroupList: NavigationGroup[],
+  children: JSX.Element[]|JSX.Element
 }
 
 const SideNav = (props: SideNavProps) => {
-
-    const [index, setIndex] = useState(0 as number);
-
-    const sideNavFontStyle = {
-        fontSize: "40px",
-        color: "#0000ff"
-    };
-    const sideNavViewStyle = {
-        width: "400px",
-        height: "100vh",
-        position: "relative",
-        padding: '30px'
-    } as React.CSSProperties;
-
-    return (
-        <div style={{
-            display: "flex",
-            justifyContent: "flex-start",
-        }}>
-            <div className={"side-nav-container"}
-                 style={{...sideNavViewStyle, ...sideNavFontStyle}}>
-                {
-                    <div>
-                        {
-                            props.navigationGroupList.map((content: NavigationGroup, i) => {
-                                return <div style={{padding: "10px", cursor: "pointer"}} onClick={() => {
-                                    setIndex(i);
-                                }}>
-                                    <span>{content.text}</span>
-                                </div>
-                            })
-                        }
-                    </div>
-                }
-            </div>
-            <div className={"main-container"} style={{width: "100%"}}>
-                {props.children}
-                {props.navigationGroupList[index].component}
-            </div>
-
+  
+  const [ index, setIndex ] = useState(0 as number);
+  const history = useHistory();
+  const sideNavFontStyle = {
+    fontSize: "40px",
+    color: "#0000ff"
+  };
+  const sideNavViewStyle = {
+    width: "400px",
+    height: "100vh",
+    position: "relative",
+    padding: '30px'
+  } as React.CSSProperties;
+  
+  return (
+    <div style={ {
+      display: "flex",
+      justifyContent: "flex-start",
+    } }>
+      <div className={ "side-nav-container" }
+           style={ {...sideNavViewStyle, ...sideNavFontStyle} }>
+        <div style={ {padding: "10px", cursor: "pointer"} }>
+          <NavLink to={ "/list" }>리스트</NavLink>
         </div>
-    )
+        <div style={ {padding: "10px", cursor: "pointer"} }>
+          <NavLink to={ "/detail" }>상세페이지</NavLink>
+        </div>
+        <div style={ {padding: "10px", cursor: "pointer"} }>
+          <NavLink to={ "/write" }>글쓰기</NavLink>
+        </div>
+      
+      </div>
+      <div className={ "main-container" } style={ {width: "100%"} }>
+        { props.children }
+      </div>
+    
+    </div>
+  )
 }
 
 export default SideNav;
